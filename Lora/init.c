3364af790a8a74c5e97122d28d5da367baf9fca1
Bùi Tuấn Anh <metwaroi31@gmail.com>
2020-09-22 20:40:39 +0700

    delay function

--- Lora/init.c
+++ Lora/init.c
@@ -3,6 +3,6 @@
 
 void init_timer_0() {
     TMOD &= 0xF0;
-    TMOD |= 0x08;
+    TMOD |= 0x09;
     TR0 = 0;
 }
--- Lora/main.c
+++ Lora/main.c
@@ -1,11 +1,14 @@
 #include <reg52.h>
 #include <init.h>
-char a = 0;
+#include <util.h>
+
 void main () {
 	
     init_timer_0();
-    
     while(1) {
-      a++;  
+      P2 = 0xFF;
+      delay(1000);
+      P2 = 0x00;
+      delay(1000);
     }
 }
